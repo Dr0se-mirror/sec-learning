7505541491ae9e53d59fed1d065836f198573096
Dr0se <2721808339@qq.com>
2024-11-18 23:10:34 +0800

    补充cc1lazy

---
+++ CC1/src/main/java/com/drose/CC1Lazymap/cc1Lazy.java
@@ -0,0 +1,16 @@
+package com.drose.CC1Lazymap;
+
+import org.apache.commons.collections.Transformer;
+import org.apache.commons.collections.functors.InvokerTransformer;
+import org.apache.commons.collections.map.TransformedMap;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class cc1Lazy {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        InvokerTransformer invokerTransformer = new InvokerTransformer("exec", new Class[]{String.class}, new Object[]{"calc"});
+    }
+}
